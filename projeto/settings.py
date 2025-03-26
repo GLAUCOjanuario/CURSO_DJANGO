277c0d27d390d8c53389204ddcf18837634e0a8a
Glauco Januario <glaucomanutencao@gmail.com>
2025-03-26 16:27:11 -0300

    criando site de receitas

--- projeto/settings.py
+++ projeto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes', # ✅ certo
 ]
 
 MIDDLEWARE = [
@@ -53,8 +55,9 @@ ROOT_URLCONF = 'projeto.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # Sobe um nível: BASE_DIR.parent
+        'DIRS': [os.path.join(BASE_DIR.parent, 'base_templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,16 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
+
+
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Pagina ', request)
+    return render(request, 'recipes/pages/home.html', status=200 , context={'name': 'Glauco Januario'}) 
 
 def sobre(request):
     return HttpResponse('Sobre', request)
 
 def contato(request):
     return HttpResponse('Contato', request)
+
